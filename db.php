1a8e95a6d1d83db4a8f2a86e06111e418c5da12d
Dima Ovharenko <dima-o@mail.ru>
2023-01-26 22:16:28 +0300

    update setting for db

--- db.php
+++ db.php
@@ -3,7 +3,10 @@ $DBHost = "localhost";
 $DBLogin = 'root';
 $DBPassword = 'root';
 $DBName = "qrcode_test";
-$conn = mysqli_connect($DBHost, $DBLogin, $DBPassword);
+$conn = mysqli_connect($DBHost, $DBLogin, $DBPassword, $DBName) or die("Connection failed: " . mysqli_connect_error());
 $select_db = mysqli_select_db($conn, $DBName);
-if(!$select_db) echo("Error connect to db");
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
 ?>
