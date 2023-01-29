af9a1c22488496e573ab051d1b994afb50a746c7
Dima Ovharenko <dima-o@mail.ru>
2023-01-29 22:58:48 +0300

    update qrcode.php fix bags

--- db.php
+++ db.php
@@ -4,7 +4,6 @@ $DBLogin = 'root';
 $DBPassword = 'root';
 $DBName = "qrcode_test";
 $conn = mysqli_connect($DBHost, $DBLogin, $DBPassword, $DBName) or die("Connection failed: " . mysqli_connect_error());
-$select_db = mysqli_select_db($conn, $DBName);
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
--- qrcode.php
+++ qrcode.php
@@ -2,11 +2,18 @@
 require_once 'db.php';
 require_once 'phpqrcode/qrlib.php';
 
+$DIR_FOR_QRCODES = 'src/images/';
+
+if (!file_exists($DIR_FOR_QRCODES)) {
+    mkdir($DIR_FOR_QRCODES);
+}
+
 if(isset($_POST["qrcode"])) {
     $qrText = $_POST["qrcode"];
-    $qrPath = 'images/'.time().'.png';
-    $qrImageName = time().'.png';
-    $query = mysqli_query($connection,"INSERT INTO qrcodes SET qrtext='$qrText', qrimage='$qrImageName'");
-    QRcode::png($qrText, $qrPath, "H", 10, 10);
-    echo "<img src='.$qrPath.' alt='QRcode image'>";
+    $qrImageName = 'qrcode_'.md5($qrText).'.png';
+    $qrPath = $DIR_FOR_QRCODES . $qrImageName;
+    $query = mysqli_query($conn, "INSERT INTO qrcodes (qrtext, qrimage) VALUES ('$qrText', '$qrImageName')");
+    QRcode::png($qrText, $qrPath);
+    echo "<div style='display: flex; flex-direction: column; gap: 10px;'><img src='/".$qrPath."' alt='QRcode image' style='width: 200px'; heigth: 200px; object-fit: contains;>";
+    echo "<a href='/'>Назад</a></div>";
 }
